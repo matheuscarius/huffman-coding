c211449eeea73b0d9c85aa86f611090ba29a2f51
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2017-05-12 12:11:51 -0300

    Add comments

--- encoding.cpp
+++ encoding.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char **argv) {
     int freq[256] = {};
 
+    //Explain usage
     if(argc == 1) {
         fprintf(stderr, "Missing argument\n");
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
@@ -16,11 +17,14 @@ int main(int argc, char **argv) {
         return 0;
     }
 
+    //Open file
     FILE *f = fopen(argv[1], "r");
     if(f == NULL) {
         fprintf(stderr, "Could not open '%s'\n", argv[1]);
         return 0;
     }
+
+    //Count frequencies
     while(1) {
         unsigned char c = fgetc(f);
         if(feof(f)) break;
